83574913381c8f429f0ef1b33ebe7a4f6aa6cd2b
unknown <gt7226@g.rit.edu>
2024-09-22 20:10:40 +0400

    pick.py

---
+++ pick.py
@@ -0,0 +1,19 @@
+''' 
+Slide 10
+
+'''
+
+def check_guess(guess, answer):
+    if guess == answer:
+        return 0
+    
+
+def test_check_guess_correct():
+    assert check_guess(5, 5) == 0 
+
+def main():
+
+    test_check_guess_correct()
+    #print(check_guess(5, 5))
+
+main()
---
+++ pick2.py
@@ -0,0 +1,37 @@
+''' Slide 11 and 12'''
+
+def check_guess(guess, answer):
+
+    difference = guess - answer
+    
+    if guess == answer:
+        print("You guessed the number!")
+        return 0
+    
+    elif guess > answer:
+        print(f"Your guess was {difference} away from the number.")
+        return difference
+    
+    else:
+        guess < answer
+        print(f"Your guess was {difference} away from the number.")
+        return difference
+
+
+
+def test_check_guess_correct():
+    assert check_guess(1, 1) == 0
+
+def test_check_guess_too_high():
+    assert check_guess(7, 5) == 2
+
+def test_check_guess_too_low():
+    assert check_guess(5, 3) == 2
+
+def main():
+    test_check_guess_correct()  
+    test_check_guess_too_high()
+    test_check_guess_too_low()
+    
+
+main()
